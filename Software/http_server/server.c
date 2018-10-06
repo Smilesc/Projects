bf36be9f530a55ae7c7cc4134ebf819bb64b5125
Sarah Miles <zaefire@gmail.com>
2018-10-06 02:41:30 -0400

    GET functional

--- Software/http_server/server.c
+++ Software/http_server/server.c
@@ -1,4 +1,3 @@
-
 // -----------------------------------
 // CSCI 340 - Operating Systems
 // Fall 2018
@@ -6,16 +5,13 @@
 // Homework 2
 //
 // -----------------------------------
-
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include "server.h"
-
 // ------------------------------------
 // Function prototype that creates a socket and
 // then binds it to the specified port_number
@@ -27,47 +23,34 @@
 //
 // Return:      Socket file descriptor (or -1 on failure)
 //
-
 int bind_port(unsigned int port_number)
 {
-
 	// -------------------------------------
 	// NOTHING TODO HERE :)
 	// -------------------------------------
 	// Please do not modify
-
 	int socket_fd;
 	int set_option = 1;
-
 	struct sockaddr_in server_address;
 
 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-
 	setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, (char *)&set_option, sizeof(set_option));
-
 	if (socket_fd < 0)
 		return FAIL;
-
 	bzero((char *)&server_address, sizeof(server_address));
-
 	server_address.sin_family = AF_INET;
 	server_address.sin_addr.s_addr = INADDR_ANY;
 	server_address.sin_port = htons(port_number);
-
 	if (bind(socket_fd, (struct sockaddr *)&server_address, sizeof(server_address)) == 0)
 	{
-
 		return socket_fd;
 	}
 	else
 	{
-
 		return FAIL;
 	}
-
 } // end bind_port function
-
-// ------------------------------------
+  // ------------------------------------
 // Function prototype that accepts a client
 // socket connection
 //
@@ -79,19 +62,12 @@ int bind_port(unsigned int port_number)
 //
 int accept_client(int server_socket_fd)
 {
-
 	int exit_status = OK;
-
 	int client_socket_fd = -1;
-
 	socklen_t client_length;
-
 	struct sockaddr_in client_address;
-
 	char request[2048];
-
 	client_length = sizeof(client_address);
-
 	client_socket_fd = accept(server_socket_fd, (struct sockaddr *)&client_address, &client_length);
 
 	// -------------------------------------
@@ -100,16 +76,10 @@ int accept_client(int server_socket_fd)
 	// Modify code to fork a child process
 	// -------------------------------------
 	int child = fork();
-	
 	if (client_socket_fd >= 0)
 	{
-		if(child > 0 || child < 0)
+		if (child == 0)
 		{
-			exit(0);
-		}
-		else
-		{
-			printf("----Forked----\n");
 			bzero(request, 2048);
 
 			read(client_socket_fd, request, 2047);
@@ -161,62 +131,61 @@ int accept_client(int server_socket_fd)
 			key = last, value = munsell
 			------------------------------------------------------
 			*/
-			static char *entity_body = 
-				"<html><body><h2>CSCI 340 (Operating Systems) Homework 2</h2><table border=1 width=\"50%\"><tr><th>Key</th><th>Value</th></tr>";
-			// char *end_type = strchr(request, ' ');
-			char *parse_mark = strchr(request, '?') + 1;
-			//char *end_pairs = strchr(request, "HTTP") - 1;
+			static char entity_body[2000] = "<html><body><h2>CSCI 340 (Operating Systems) Homework 2</h2><table border=1 width=\"50%\"><tr><th>Key</th><th>Value</th></tr>";
+			char *end_type = strchr(request, ' ');
+			char *begin_pairs = strchr(request, '?');
+			char type[end_type - request];
+			strncpy(type, request, end_type - request);
 
-			//char url_only[end_pairs - parse_mark];
-			//strncpy(url_only, parse_mark + 1, end_pairs - parse_mark);
-
-			// char type[end_type - request];
-			// strncpy(type, request, end_type - request);
-
-			//char *token_end;
-
-			char *amp = "&";
-			char *equals = "=";
-			char *space = " ";
-
-			printf("TYPE: %c\n", request[0]);
-			//printf("request[1]: %c\n", request[1]);
-			if (request[0] == 71)
+			printf("TYPE: %s\n", type);
+			if (strcmp(type, "GET") == 0)
 			{
-				printf("in if******\n");
-				static char *token;
-				
+				char *token_start = begin_pairs + 1;
 
-				//char parse_item = 0;
-				printf("did statements\n");
-				while(strcmp(parse_mark, space) != 0)
-				{
-					//int parse_item = *parse_mark;
-					
-					printf("in for\n");
-					strcat(token, parse_mark);
+				while (1)
+				{	
+					//find end of key/value pair
+					char *token_end = strchr(token_start, '&');
+					if (token_end == NULL)
+					{
+						token_end = strchr(token_start, ' ');
+					}
 
-					if(strcmp(parse_mark, equals) == 0)
+					//extract key
+					char *key_end = strchr(token_start, '=');
+					int key_len = key_end - token_start;
+					char key[key_len];
+					strncpy(key, token_start, key_len);
+					key[key_len] = '\0';
+
+					//extract value
+					char value[token_end - key_end];
+					int val_len = token_end - key_end;
+					strncpy(value, (key_end + 1), val_len);
+					value[val_len] = '\0';
+
+					//HTML things here
+					strcat(entity_body, "<tr><td>");
+					strcat(entity_body, key);
+					strcat(entity_body, "</td><td>");
+					strcat(entity_body, value);
+					strcat(entity_body, "</td></tr>");
+
+					token_start = token_end + 1;
+					printf("entity_body: %s\n", entity_body);
+
+					if (token_end[0] == ' ')
 					{
-						sprintf(entity_body, "<tr><td>%s</td>", token);
-						free(token);
+						break;
 					}
-					if(strcmp(parse_mark, amp) == 0)
-					{
-						sprintf(entity_body, "<td>%s</td></tr>", token);
-						free(token);
-					}
-					parse_mark++;
 				}
-				printf("after while\n");
 				strcat(entity_body, "</table></body></html>");
 			}
-			else if (request[0] == 80)
+			else if (strcmp(type, "POST") == 0)
 			{
 			}
-
 			// THIS IS AN EXAMPLE ENTITY BODY
-			//char *entity_body = "<html><body><h2>CSCI 340 (Operating Systems) Homework 2</h2><table border=1 width=\"50%\"><tr><th>Key</th><th>Value</th></tr></table></body></html>";
+			//char* entity_body = "<html><body><h2>CSCI 340 (Operating Systems) Homework 2</h2><table border=1 width=\"50%\"><tr><th>Key</th><th>Value</th></tr></table></body></html>";
 
 			char response[512];
 			sprintf(response, "HTTP/1.1 200 OK\r\nContent-Length: %d\r\n\r\n%s", (int)strlen(entity_body), entity_body);
