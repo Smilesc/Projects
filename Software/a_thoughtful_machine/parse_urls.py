7aae58991c2f6007ab6ba97fb149c1235612f306
Smilesc <milessc@gmail.com>
2019-02-05 00:29:32 -0500

    Add image url grab

--- Software/a_thoughtful_machine/parse_urls.py
+++ Software/a_thoughtful_machine/parse_urls.py
@@ -1,13 +1,22 @@
-import urllib
+import urllib.request
+from PIL import Image
+from resizeimage import resizeimage
+
 
 url_file = open("Image_URLS.csv")
 o = open("HTTP_Image_URLS.txt", "a")
 
 URLS = []
+img_num = 0
 for url in url_file:
     new_url = url.replace("https", "http")
     URLS.append(new_url)
+    o.write(new_url)
 
-    # o.write(new_url)
+    # img_file = "img" + str(img_num) + ".jpg"
+    # urllib.request.urlretrieve(new_url, img_file)
+    
+    # img = open(img_file)
+    # resizeimage.resize_height(img, 512)
 
-urllib.request.urlretrieve
+    img_num += 1
