a5329cb6aa5e710f4a823b4e15488f50aba46b9f
Sarah Miles <zaefire@gmail.com>
2019-04-18 21:10:41 -0400

    Remove 25, add hsv code

---
+++ Software/a_thoughtful_machine/combine_joblibs.py
@@ -0,0 +1,17 @@
+from sklearn.externals import joblib
+
+
+with open("joblibs/IMG_list_train_all_hsv1.joblib", 'rb') as jl:
+    hsv1 = joblib.load(jl)
+
+    print("hsv1: ", len(hsv1))
+    print(hsv1[0])
+
+# with open("joblibs/IMG_list_train_all_hsv1.joblib", 'rb') as jl:
+#     hsv1 = joblib.load(jl)
+
+#     print("hsv: ", len(hsv), " hsv1:", len(hsv1))
+#     hsv.extend(hsv1)
+
+# with open("joblibs/IMG_list_train_all_hsv_all.joblib","wb") as p:
+#     joblib.dump(hsv, p)
--- Software/a_thoughtful_machine/create_joblib.py
+++ Software/a_thoughtful_machine/create_joblib.py
@@ -1,31 +1,35 @@
-import urllib.request
 from PIL import Image
-from resizeimage import resizeimage
-import pickle
 from sklearn.externals import joblib
 import colorsys
+import numpy as np
 
 IMG_list=[]
-img_num = 1
+img_num = 616
 
-for _ in range(1232):
+for m in range(615):
+    if m != 24:
+        print(m)
+        #construct image file name
+        img_file = "img" + str(img_num) + ".jpg"
 
-    #construct image file name
-    img_file = "img" + str(img_num) + ".jpg"
+        img = Image.open("images/200x200s/" + img_file)
 
-    img = Image.open("images/200x200s/" + img_file)
+        #convert image to rgb values and flatten
+        img_px = list(img.getdata())
 
-    #convert image to rgb values and flatten
-    img_px = list(img.getdata())
-    hsv_img_px = []
-    for rgb in img_px:
-        hsv_img_px.append(colorsys.rgb_to_hsv(rgb[0], rgb[1], rgb[2]))
-    img_px_flat = [round(x, 3) for sets in hsv_img_px for x in sets]
+        if len(img_px[0]) == 4:
+            print("AAAAAAAAAAAAAAAAAAAAAAAAAAAA", img_num, img_px[0])
 
-    #add image to list
-    IMG_list.append(img_px_flat)
+
+        #add image to list
+        IMG_list.append([x for sets in img_px for x in sets])
 
     img_num += 1
 
-with open("joblibs/IMG_list_train_all_hsv.joblib","wb") as p:
+for j, image in enumerate(IMG_list):
+    for i in range(0, len(image), 3):
+        h, s, v = colorsys.rgb_to_hsv(image[i], image[i + 1], image[i + 2])
+        IMG_list[j][i], IMG_list[j][i + 1], IMG_list[j][i + 2] = round(h, 3), round(s, 3), round(v, 3)
+
+with open("joblibs/IMG_list_train_all_hsv1.joblib","wb") as p:
     joblib.dump(IMG_list, p)
